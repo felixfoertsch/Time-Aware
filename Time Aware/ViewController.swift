059b09c76c17f01a598a391da3be35a98d4a2857
Felix Förtsch <felixfoertsch@gmail.com>
2019-01-26 13:51:53 +0100

    add project files

---
+++ Time Aware/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Aware
+//
+//  Created by Felix Förtsch on 26.01.19.
+//  Copyright © 2019 Felix Förtsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
